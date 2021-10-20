1d2d2d3d02fa73195bf086747f8365ed61ea8fa0
zerulegend <zerulegend@gmail.com>
2021-10-20 13:17:58 +0700

    Create / Post User Data

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,5 @@
 export default {
     port: 4000,
-    dbUri: "mongodb+srv://zeniaMongo:zeniaAdmin@zeniacluster.iihdj.mongodb.net/satgasCovid"
+    dbUri: "mongodb+srv://zeniaMongo:zeniaAdmin@zeniacluster.iihdj.mongodb.net/satgasCovid",
+    saltWorkFactor: 10
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,12 @@
 import { Express, Request, Response } from 'express'
+import {createUserHandler} from "./controller/user.controller";
+import validateResource from "./middleware/validateResource";
+import {createUserScema} from "./schema/user.schema";
 
 function routes(app: Express) {
     app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200))
+
+    app.post('/api/users', validateResource(createUserScema),createUserHandler)
 }
 
 export default routes
